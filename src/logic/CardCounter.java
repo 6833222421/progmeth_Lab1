23973211d27b84a3b08272769e3670c7785c2bf6
Attasit <monjung888@gmail.com>
2026-01-16 22:24:03 +0700

    Implement CardCounter constructor, getters/setters, and count validation.

--- src/logic/CardCounter.java
+++ src/logic/CardCounter.java
@@ -1,12 +1,28 @@
 package logic;
 
 public class CardCounter {
-	
-	private UnitCard card;
-	private int count;
-	
-	public String toString() {
-		return  this.getCard() + " x " + this.getCount();
-	}
+
+    private UnitCard card;
+    private int count;
+    public CardCounter(UnitCard card,int count){
+        setCard(card);
+        setCount(count);
+    }
+    public void setCount(int count){
+        this.count=Math.max(0,count);
+    }
+    public void setCard(UnitCard card){
+        this.card=card;
+    }
+    public UnitCard getCard(){
+
+        return this.card;
+    }
+    public int getCount(){
+        return  this.count;
+    }
+    public String toString() {
+        return  this.getCard() + " x " + this.getCount();
+    }
 
 }
